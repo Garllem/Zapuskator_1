6c9237f534062ca8ff7fd6fa2e2af999b5118141
Garllem <garllem@gmail.com>
2023-05-03 23:37:53 +0300

    1 вариант

--- Zapuskator_1.py
+++ Zapuskator_1.py
@@ -1,8 +1,18 @@
 import PySimpleGUI as sg
 import os
+import json
+
+# Определяем путь к папке с программой
+program_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Создаем список последних выбранных программ
 last_programs = ['', '', '']
+
+# Загружаем список последних выбранных программ из файла, если он существует
+last_programs_file = os.path.join(program_dir, 'last_programs.json')
+if os.path.exists(last_programs_file):
+    with open(last_programs_file, 'r') as f:
+        last_programs = json.load(f)
 
 # Создаем список типов файлов для отображения в диалоговом окне выбора файлов
 filetypes = [("All files", "*.*")]
@@ -17,7 +27,7 @@ layout = [
 ]
 
 # Создаем окно с графическим интерфейсом
-window = sg.Window("Program Runner", layout)
+window = sg.Window("KMA Runner", layout)
 
 # Обрабатываем события окна
 while True:
@@ -35,6 +45,10 @@ while True:
         for program in last_programs:
             if program:
                 os.startfile(program)
-
+        
+        # Сохраняем список последних выбранных программ в файл
+        with open(last_programs_file, 'w') as f:
+            json.dump(last_programs, f)
+            
 # Закрываем окно
 window.close()
